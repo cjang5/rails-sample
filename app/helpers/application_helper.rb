b642770ac39e2792f05d6f6464f1d3c98b5f43bb
cjang5 <chrismangggg@gmail.com>
2016-01-14 22:19:15 -0500

    Added full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# return full title for each page
+	def full_title(page_title = '')
+		# default title
+		base_title = "Sample App"
+
+		# If there is no title for the page, just use base_title
+		if page_title.empty?
+			base_title # return base_title
+		else
+			"#{page_title} | #{base_title}" # return concatenation of page+base titles
+		end
+	end
 end
